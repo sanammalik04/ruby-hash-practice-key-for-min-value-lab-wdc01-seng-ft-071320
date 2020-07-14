331c6fffb5e4991d3b87a13a85627a5dc67112e2
Sanam Malik <sanam.malik04@gmail.com>
2020-07-14 19:38:03 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,18 +5,3 @@ def key_for_min_value(name_hash)
 
 end
 
-
-def key_for_min_value(name_hash)
-  if name_hash.length == 0 then
-    return NIL
-  end
-  minimum = Float::INFINITY
-  returnValue = ""
-  name_hash.each do |key, value|
-    if value < minimum then 
-      minimum = value
-      returnValue = key
-    end
-  end
-  return returnValue
-end
